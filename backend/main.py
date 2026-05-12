173524f3084e48ac321c73cb5c39ab6d67617f0f
bastet <ervanursahin1996@gmail.com>
2026-05-12 14:34:05 +0300

    gemini a api kısmı halloldu. gemini yorumları analiz ediyor yorumları analiz etmesi için promptu sonradan güçlendiririz.

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from services.gemini_service import analyze_review
+
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-class Review(BaseModel):
-    review : str
+# Frontend'in erişimine izin ver
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Geliştirme aşamasında her yerden erişime izin veriyoruz
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class ReviewInput(BaseModel):
+    review: str
+
+
 @app.post("/analyze-review")
-def analyze_review(review : Review):
-    
-    
+def analyze(data: ReviewInput):
+    return analyze_review(data.review)
